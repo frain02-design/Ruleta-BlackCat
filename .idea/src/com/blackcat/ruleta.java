706f70ad75116bbdd5ee7271cfd36cb14199edc2
GatoMiau03 <d.benaldo03@ufromail.cl>
2026-03-26 09:59:57 -0300

    Se añadio menu principal y lectura de apuestas

--- .idea/src/com/blackcat/ruleta.java
+++ .idea/src/com/blackcat/ruleta.java
@@ -17,16 +17,76 @@ public class ruleta {
         menu();
     }
 
-    public static void menu() {}
-    public static void mostrarMenu() {}
-    public static int leerOpcion(Scanner in) { return 0; }
-    public static void ejecutarOpcion(int opcion, Scanner in) {}
-    public static void iniciarRonda(Scanner in) {}
-    public static char leerTipoApuesta(Scanner in) { return ' '; }
+
+    public static void menu() {
+        Scanner in = new Scanner(System.in);
+        int opcion;
+        do {
+            mostrarMenu();
+            opcion = leerOpcion(in);
+            ejecutarOpcion(opcion, in);
+        } while (opcion != 0);
+        in.close();
+    }
+
+    public static void mostrarMenu() {
+        System.out.println("\n=== CASINO BLACK CAT - RULETA ===");
+        System.out.println("1. Iniciar ronda");
+        System.out.println("2. Ver estadísticas");
+        System.out.println("0. Salir");
+        System.out.print("Opción: ");
+    }
+
+    public static int leerOpcion(Scanner in) {
+        return in.nextInt();
+    }
+
+    public static void ejecutarOpcion(int opcion, Scanner in) {
+        switch (opcion) {
+            case 1:
+                iniciarRonda(in);
+                break;
+            case 2:
+                mostrarEstadisticas();
+                break;
+            case 0:
+                System.out.println("¡Gracias por jugar!");
+                break;
+            default:
+                System.out.println("Opción no válida");
+        }
+    }
+
+    // ========== RONDA ==========
+    public static void iniciarRonda(Scanner in) {
+        System.out.println("\n--- NUEVA RONDA ---");
+        char tipo = leerTipoApuesta(in);
+        int monto = leerMonto(in);
+        System.out.println("Apuesta: " + tipo + " - $" + monto);
+    }
+
+    public static int leerMonto(Scanner in) {
+        System.out.print("Monto a apostar: $");
+        return in.nextInt();
+    }
+
+    public static char leerTipoApuesta(Scanner in) {
+        char tipo;
+        do {
+            System.out.print("Tipo (R=Rojo, N=Negro, P=Par, I=Impar): ");
+            tipo = in.next().toUpperCase().charAt(0);
+        } while (tipo != 'R' && tipo != 'N' && tipo != 'P' && tipo != 'I');
+        return tipo;
+    }
+
+    // ========== FUNCIONES PENDIENTES ==========
     public static int girarRuleta() { return 0; }
     public static boolean evaluarResultado(int numero, char tipo) { return false; }
     public static boolean esRojo(int n) { return false; }
     public static void registrarResultado(int numero, int apuesta, boolean acierto) {}
     public static void mostrarResultado(int numero, char tipo, int monto, boolean acierto) {}
-    public static void mostrarEstadisticas() {}
+
+    public static void mostrarEstadisticas() {
+        System.out.println("\nEstadísticas (próximamente)");
+    }
 }
